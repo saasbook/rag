c40b53dd2f535882328ba9bbfd53b54d7e49b91c
Richard Xia <rxia@eecs.berkeley.edu>
2012-04-03 12:28:30 -0700

    Made feature safer

--- hw4_grader_stuff/hw4_rottenpotatoes/features/step_definitions/saas_web_steps.rb
+++ hw4_grader_stuff/hw4_rottenpotatoes/features/step_definitions/saas_web_steps.rb
@@ -1,6 +1,11 @@
 Then /^(?:|I )should either be on (.+) or (.+)$/ do |page_name, page_name2|
   current_path = URI.parse(current_url).path
-  paths = [path_to(page_name), path_to(page_name2)]
+  paths = []
+  begin
+    paths << path_to(page_name)
+    paths << path_to(page_name2)
+  rescue RuntimeError
+  end
   if current_path.respond_to? :should
     paths.should include current_path
   else
