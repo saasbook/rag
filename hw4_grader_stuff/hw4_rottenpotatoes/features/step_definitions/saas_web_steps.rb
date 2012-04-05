10d44a94205bd5ebb77ac0a85fd641902a86252a
Richard Xia <rxia@eecs.berkeley.edu>
2012-04-05 12:01:26 -0700

    Installed cucumber raketask on default repo. Fixed saas_web_steps.rb

--- hw4_grader_stuff/hw4_rottenpotatoes/features/step_definitions/saas_web_steps.rb
+++ hw4_grader_stuff/hw4_rottenpotatoes/features/step_definitions/saas_web_steps.rb
@@ -1,17 +1,7 @@
 Then /^(?:|I )should either be on (.+) or (.+)$/ do |page_name, page_name2|
-  current_path = URI.parse(current_url).path
-  paths = []
   begin
-    paths << path_to(page_name)
-  rescue RuntimeError
-  end
-  begin
-    paths << path_to(page_name2)
-  rescue RuntimeError
-  end
-  if current_path.respond_to? :should
-    paths.should include current_path
-  else
-    assert paths.include?(current_path)
+    step %Q{I should be on #{page_name2}}
+  rescue Cucumber::Undefined => e
+    step %Q{I should be on #{page_name}}
   end
 end
