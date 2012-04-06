036f65e7818b5180d7f0ceb47a94641d2d9c21a4
Richard Xia <rxia@eecs.berkeley.edu>
2012-04-06 11:17:54 -0700

    Made step better

--- hw4_grader_stuff/hw4_rottenpotatoes/features/step_definitions/saas_web_steps.rb
+++ hw4_grader_stuff/hw4_rottenpotatoes/features/step_definitions/saas_web_steps.rb
@@ -1,7 +1,16 @@
 Then /^(?:|I )should either be on (.+) or (.+)$/ do |page_name, page_name2|
   begin
-    step %Q{I should be on #{page_name2}}
-  rescue Cucumber::Undefined => e
     step %Q{I should be on #{page_name}}
+  #rescue Cucumber::Undefined => e
+  #  step %Q{I should be on #{page_name2}}
+  #rescue
+  #  err_msg = $!.to_s
+  #  if err_msg =~ /^Can't find mapping from ".*" to a path\.$/
+  #    step %Q{I should be on #{page_name2}}
+  #  else
+  #    raise
+  #  end
+  rescue
+    step %Q{I should be on #{page_name2}}
   end
 end
