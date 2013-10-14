de2c1352289998d44dd6b05c86d68ad5b8146f11
Sam Joseph <tansaku@gmail.com>
2013-10-14 13:47:41 +0100

    adding some extra details for testing the homeworks and config for Oracle of bacon assignment

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -15,3 +15,19 @@ bundle exec rspec
 
 cd ..
 git clone https://github.com/saasbook/hw.git
+
+cd hw
+bundle install
+./check_all_solutions
+
+cd ..
+cd rag
+cd spec
+
+# required to allow Oracle of Bacon grader to run ...
+ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/graph_example.xml
+ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/graph_example2.xml
+ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/spellcheck_example.xml
+ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/unauthorized_access.xml
+ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/unknown.xml
+
