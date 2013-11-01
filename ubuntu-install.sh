e921a4549e72c117c0877d1054a0dc703cd05630
Sam Joseph <tansaku@gmail.com>
2013-11-01 21:48:12 +0000

    latest updates to the install script - still needs confirmation on a fresh box ... do we really need spork?

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y curl
 \curl -L https://get.rvm.io | bash -s stable  --ruby=1.9.3
 source ~/.rvm/scripts/rvm
 
+# this needed for HW4 and must be installed before some other versions of libv8 are pulled in?
+gem install therubyracer -v '0.9.10'
+
 git clone https://github.com/saasbook/rag.git
 
 cd rag
@@ -30,4 +33,38 @@ ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/graph_example2.xml
 ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/spellcheck_example.xml
 ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/unauthorized_access.xml
 ln -s /home/ubuntu/hw/oracle-of-bacon/public/spec/unknown.xml
+
+#
+cd ../..
+
+# To get a nice footer for screen for .screenrc
+
+cp rag/.screenrc .screenrc
+
+# to set up HW3 we must import the gemset
+# this should be done in a second screen window
+
+cd hw/bdd-cucumber/public/rottenpotatoes/
+/bin/bash --login
+rvm gemset create rag3
+rvm gemset import rag3
+rvm gemset use rag3
+
+# also we need:
+
+cd ~
+sudo apt-get install libxslt-dev libxml2-dev
+git clone https://github.com/saasbook/rottenpotatoes.git
+cd rottenpotatoes/
+git checkout -b hw3_solution origin/hw3_solution
+bundle install
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:test:prepare
+
+# then the spork command can be run
+bundle exec spork cucumber
+
+
+
 
