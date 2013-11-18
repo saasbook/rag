5ac0d449d29a2779b701833b0839d9732bb80c81
Nidhin Pattaniyil <npatta01@gmail.com>
2013-11-18 14:30:35 -0500

    started creating cucumber feature for testing late submission

--- features/step_definitions/subject_code_steps.rb
+++ features/step_definitions/subject_code_steps.rb
@@ -1,9 +1,39 @@
 require 'tempfile'
 
 
-Given(/^a configuration file with a grace period of "(.*?)" and a late period of "(.*?)"$/) do |grace, late|
-  pending # express the regexp above with the code you wish you had
+Given(/^a configuration file with a grace period of "(.*?)" and a late period of "(.*?)" and assignment date of "(.*?)"$/) do |grace, late,duedate|
+
+  generateAutoConfigFile(grace,late,duedate)
 end
+
+And(/^a student submits an assignment "(.*?)" days late$/) do |days|
+  EdXClient.init_autograders('./config/autograders.yml')
+  #client = EdXClient.new()
+
+  file = Tempfile.new('test-submission')
+  file.write %Q{
+    class MyClass
+      def self.my_method
+        #{code}
+        return 'foo'
+      end
+    end
+  }
+  file.flush
+  submission_path = file.path
+
+
+  score, comments =AutoGraderSubprocess.run_autograder_subprocess(submission_path, spec, grader_type)
+
+  client.
+  #submitted_date=Date.today+days
+  # @output=""
+
+
+end
+
+
+
 
 Given /^a submission containing "(.*)"$/ do |code|
   file = Tempfile.new('cucumber-code')
@@ -62,4 +92,29 @@ When /^I run the ruby intro grader for "(.*?)"$/ do |homework_number|
   end
 
   @output = `ruby #{$APP}/grade #{@codefile} #{specfile}`
+end
+
+
+def generateAutoConfigFile (graceDays,lateDays, dueDays)
+
+
+  due_date = Date.today.strftime("%Y%m%d%H%M%S")
+
+  file = File.open('config/autograders.yml',"w")
+  file.write %Q{
+  assign-0-queue:
+  name: "test-pull"
+  type: WeightedRspecGrader
+  due : #{due_date}
+  grace_period: #{graceDays}
+  late_period: #{lateDays}
+  parts:
+    assign-0-part-1:
+      uri: ../hw/solutions/part1_spec.rb
+      type: WeightedRspecGrader
+  }
+
+  file.flush
+  @codefile = file.path
+
 end
--- features/student_feedback.feature
+++ features/student_feedback.feature
@@ -5,7 +5,7 @@ Feature: Feedback for Students
 
   Scenario: correctly report late penalty
 
-    Given a configuration file with a grace period of "1" and a late period of "3"
+    Given a configuration file with a grace period of "1" and a late period of "3" and assignment date of "3"
     And a student submits an assignment "5" days late
     Then the student should see a "5" days late message
 
