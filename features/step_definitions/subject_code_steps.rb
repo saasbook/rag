202cddb9175c4c93a0abddce3273b9041ed4eda7
mtc2013 <mtc2013opensource@gmail.com>
2013-11-25 13:08:24 -0500

    Worked out scenarios for each period in question for grader messages

--- features/step_definitions/subject_code_steps.rb
+++ features/step_definitions/subject_code_steps.rb
@@ -7,13 +7,24 @@ Given(/^a configuration file with a grace period of "(.*?)" and a late period of
   generateAutoConfigFile(grace,late,duedate)
 end
 
-And(/^a student submits an assignment on "(.*?)" and gets a no credit period message$/) do |submission_time|
+And(/^a student submits an assignment on "(.*?)" and gets a "(.*?)" period message$/) do |submission_time, period|
   #EdXClient.init_autograders('./config/autograders.yml')
+
+  message = case period
+    when "no credit"
+      /^<pre>More than \d+ day\(s\) late:/
+    when "grace"
+      /^<pre>Late assignment: score scaled by .75/
+    when "late"
+      /^<pre>It's less than \d+ day\(s\) late: score scaled by: .5/
+    else
+      /^Something not likely to be in the message/
+  end
   controller_mock = double("EdXController")
   controller_mock.should_receive(:send_grade_response) do |checkmark, score, comment|
     checkmark.should be_false
     score.should eq 0
-    comment.should =~ /^<pre>More than \d+ day\(s\) late:/
+    comment.should =~ message
   end
   EdXController.stub(:new).and_return controller_mock
   code = %Q{
--- features/student_feedback.feature
+++ features/student_feedback.feature
@@ -6,6 +6,14 @@ Feature: Feedback for Students
   Scenario: correctly report no credit period
 
     Given a configuration file with a grace period of "1" and a late period of "3" and assignment date of "20131010235959"
-    And a student submits an assignment on "20131015235959" and gets a no credit period message
+    And a student submits an assignment on "20131015235959" and gets a "no credit" period message
+
+  Scenario: correctly reports grace period
+    Given a configuration file with a grace period of "1" and a late period of "2" and assignment date of "20131010235959"
+    And a student submits an assignment on "20131011010000" and gets a "grace" period message
+
+   Scenario: correctly reports late period
+     Given a configuration file with a grace period of "1" and a late period of "2" and assignment date of "20131010235959"
+     And a student submits an assignment on "20131012015959" and gets a "late" period message
 
 
