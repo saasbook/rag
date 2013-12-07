d6f98c5eb0eac249e3be525391abbe8e44bca3d9
mtc2013 <mtc2013opensource@gmail.com>
2013-12-06 19:11:54 -0500

    Stubbed out file system for spec

--- spec/feature_grader_spec.rb
+++ spec/feature_grader_spec.rb
@@ -5,6 +5,9 @@ describe FeatureGrader do
     lambda { FeatureGrader.new('foo', {:description => 'hw3.yml'}) }.should raise_error ArgumentError, /features/
   end
   it "should give error when initializing and it cannot find the features archive file" do
-    lambda { FeatureGrader.new('foo', {:description => 'none.yml'}) }.should raise_error ArgumentError, /features/
+    File.should_receive(:file?).with('foo').and_return true
+    File.should_receive(:readable?).with('foo').and_return true
+    File.should_receive(:file?).with('none.yml').and_return false
+    lambda { FeatureGrader.new('foo', {:description => 'none.yml'}) }.should raise_error ArgumentError, /description/
   end
 end
