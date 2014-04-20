28d1a4f6c05b072d50674fab63dd350e392ab037
Paul McCulloch <hrfeels@gmail.com>
2014-04-20 11:33:32 -0700

    16 todos

--- lib/graders/rspec_grader/rails_intro_archive_grader.rb
+++ lib/graders/rspec_grader/rails_intro_archive_grader.rb
@@ -6,15 +6,18 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
     @archive = archive
     host = '127.0.0.1' #TODO load config from yml file?
     port = '3000' #TODO make it other than port 3000
-    # TODO why can't change name of this variable? super?
+    # super field
     @heroku_uri = 'http://' + host + ':' + port
   end
 
   def grade!
-    ENV['HEROKU_URI'] = @heroku_uri
+    kill_port_process!
+
+    #ENV['HEROKU_URI'] = @heroku_uri
     #TODO kill anything on our port
     #TODO log it, log everything
     #TODO grader name
+
     Dir.mktmpdir('rails_intro_archive', '/tmp') do |tmpdir|
       @temp = tmpdir
       #TODO run_process on it?
@@ -28,11 +31,15 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
       rails_up_timeout(30)
 
       super
+
+      #TODO DRY it with kill_port_process!
       #TODO run_process on it?
-      pid = `pgrep -f "ruby script/rails s"`
-      #TODO repetitive
-      Process.kill('INT', pid.to_i)
-      Process.kill('KILL', pid.to_i)
+      pid = `pgrep -f "ruby script/rails s"`.to_i
+      #pid = `$(lsof -wni tcp:3000 |  xargs echo | cut -d \\  -f 11)`.to_i
+      if pid > 0
+        Process.kill('KILL', pid) unless Process.kill('INT', pid) == 1
+      end
+
     end
   end
 
@@ -48,6 +55,16 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
           @status.to_s) unless @status.success? and @errors == ''
   end
 
+
+  def kill_port_process!
+    pid = `lsof -wni tcp:3000 | xargs echo | cut -d ' ' -f 11`.to_i
+    #rails_pid = `pgrep -f "ruby script/rails s"`.to_i
+    ##TODO log alert if these two numbers differ
+    if pid > 0
+      Process.kill('KILL', pid) unless Process.kill('INT', pid) == 1
+      #TODO raise process can't be killed unless stopped
+    end
+  end
 
   #TODO make timeout bulletproof
   def rails_up_timeout(sec, polling=1)
@@ -65,7 +82,7 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
     begin
       #uri = URI.parse("") => test
       #uri = URI.parse("#{@heroku_uri}/steez") => test causes timeout unless rescue
-      uri = URI.parse("#{@heroku_uri}")
+      uri = URI.parse(@heroku_uri)
       response = Net::HTTP.get_response(uri)
       return true if response.respond_to?(:code) and response.code != nil
     rescue Errno::ECONNREFUSED
