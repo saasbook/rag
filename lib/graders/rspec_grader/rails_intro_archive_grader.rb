512f46655b664631d0e9e92824478a9d426dbbc6
Paul McCulloch <hrfeels@gmail.com>
2014-04-20 00:14:14 -0700

    rearrange methods

--- lib/graders/rspec_grader/rails_intro_archive_grader.rb
+++ lib/graders/rspec_grader/rails_intro_archive_grader.rb
@@ -8,36 +8,6 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
     @archive = archive
   end
 
-  def self.run_process(cmd, dir)
-      @output, @errors, @status = Open3.capture3(
-          cmd, :chdir => dir
-      )
-      puts (cmd +
-          @output +
-          @errors +
-          @status.to_s) unless @status.success? and @errors != ''
-  end
-
-  def app_loaded?
-    begin
-      require 'net/http'
-      uri = URI.parse("http://127.0.0.1:3000/movies/")
-      response = Net::HTTP.get_response(uri)
-      if response and response.code
-        return true if response.respond_to?(:code) && response.code == '200'
-      end
-    rescue Errno::ECONNREFUSED
-      return false
-    end
-    false
-  end
-
-  def wait_for_app_max(sec, polling=1)
-    to_status = timeout(sec) {
-      sleep(polling) until app_loaded?
-    }
-  end
-
   def grade!
     ENV['HEROKU_URI'] = @heroku_uri
     Dir.mktmpdir('rails_intro_archive', '/tmp') do |tmpdir|
@@ -47,12 +17,41 @@ class RailsIntroArchiveGrader < HerokuRspecGrader
       pid = Process.fork do
         run_process('rails s', @temp)
       end
-      wait_for_app_max(30)
+      rails_up_timeout(30)
       super
       pid = `pgrep -f "ruby script/rails s"`
       Process.kill('INT', pid.to_i)
       Process.kill('KILL', pid.to_i)
     end
   end
+
+  def run_process(cmd, dir)
+      @output, @errors, @status = Open3.capture3(
+          cmd, :chdir => dir
+      )
+      puts (cmd +
+          @output +
+          @errors +
+          @status.to_s) unless @status.success? and @errors == ''
+  end
+
+  def rails_up_timeout(sec, polling=1)
+    to_status = timeout(sec) {
+      sleep(polling) until app_loaded?
+    }
+  end
+
+  def app_loaded?
+    begin
+      require 'net/http'
+      uri = URI.parse("http://127.0.0.1:3000/movies/")
+      response = Net::HTTP.get_response(uri)
+      return true if response.respond_to?(:code) and response.code != nil
+    rescue Errno::ECONNREFUSED
+      return false
+    end
+    false
+  end
+
 end
 
