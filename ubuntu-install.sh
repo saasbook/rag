bbd41f70e9c91ad73d88b7fce5e57d1625dd5fd5
Paul McCulloch <hrfeels@gmail.com>
2014-05-14 05:32:39 +0000

    add check for screens, read info, and added clears which could get rid of

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,48 +1,93 @@
 #!/bin/bash
 # Simple autograder setup.sh for configuring Ubuntu 14.04 LTS EC2 instance
+
+
 SPORK_SCREEN=Spork
 EDX_CLIENT_SCREEN=Edx-client
+SCROLLBACK_DEFAULT=500
 RAG_BRANCH=Spring2014
 HW_BRANCH=Spring2014
-#TODO add Spring2014 branch for this repo too, and tag deploy on success
 ROTTENPOTATOES_BRANCH=hw3_solution
+RUBYGEMS_VERSION=2.2.2
+clear
+sleep 1
 
+
+
+
+echo "
+         Install Deployment Script for Saasbook to EC2
+-------------------------------------------------------------------------------
+"
+if [ -z "$(echo $(screen -ls) | grep 'No Sockets')" ]; then
+  echo "It seems some screen is running, you better check it out."
+  screen -ls
+  echo
+  echo "Ctrl+c to exit and 'screen -r' to reattach and examine them."
+  echo "If not concerned about exit, 'pkill -f SCREEN' kills all."
+  read -p "Enter will continue if you wish, however."
+fi
 echo "
  0. Get a copy of the old rag/config/autograders.yml and conf.yml for later.
  1. Copy .screenrc and this file from rag to rag/.. and do 'source .screenrc'
- 2. './ubuntu-install.sh'
+ 2. Execute this script: './ubuntu-install.sh' ($0)
  3. 'sudo chmod a+x ubuntu-install.sh' if execute permission denied.
  4. Enter sudo password.
- 5. Enter github credentials at clone of saasbook/hw and saasbook/rottenpotatoes.
- 6. When prompted, after install of rag, copy over old config files and edit.
+ 5. Enter github credentials, screen scrollback.
+ 6. When prompted, copy over old config files and edit.
  7. When running, access with 'screen -r $SPORK_SCREEN' and 'screen -r $EDX_CLIENT_SCREEN'
  8. If the script is re-run it shows errors but seems to be OK.
- 9. If run multiple times, consider 'screen -r' and then 'Ctrl+a k' to kill them.
+ 9. If run multiple times, you must kill the extra screen processes.
 10. Check courseware is same queue and assignments as rag/config/autograder.yml
 "
-read -p "* Click Enter key to continue, Ctrl+c to exit.. "
-read -p "* GitHub username for private repos: " GH_USER
-read -s -p "* GitHub password: " GH_PASS
+read -p "* Enter scrollback for screen. [$SCROLLBACK_DEFAULT]: " SCROLLBACK
+SCROLLBACK="${SCROLLBACK:-$SCROLLBACK_DEFAULT}"
+read -p "* Enter GitHub username for private repos: " GH_USER
+read -s -p "* Enter GitHub password: " GH_PASS
+clear
+
+INSTALL_ARGS="
+---------------------------------
+Install to PWD: $PWD
+RUBYGEMS_VERSION: $RUBYGEMS_VERSION
+GH_USER: $GH_USER
+GH_PASS: -secret-
+RAG_BRANCH: $RAG_BRANCH
+HW_BRANCH: $HW_BRANCH
+ROTTENPOTATOES_BRANCH: $ROTTENPOTATOES_BRANCH
+SPORK_SCREEN: $SPORK_SCREEN
+EDX_CLIENT_SCREEN: $EDX_CLIENT_SCREEN
+SCROLLBACK: $SCROLLBACK
+---------------------------------
+"
+echo "
+     Review install options:$INSTALL_ARGS"
+read -p "* If above info is correct, click Enter key to continue, or Ctrl+c to exit and edit $0 "
+
+
+
+
 
 echo "
-############################## Install Dependencies ############################
+############################## Install Dependencies ###########################
 "
 
+gem update --system $RUBYGEMS_VERSION
 sudo apt-get install -y git
 sudo apt-get install -y curl
-
 \curl -L https://get.rvm.io | bash -s stable  --ruby=1.9.3
 source ~/.rvm/scripts/rvm
-
 # this needed for HW4 and must be installed before some other versions of libv8 are pulled in?
 # We can get rid of it if we update HW3 and 4. It appears to fail if already installed, but tests pass.
 gem install therubyracer -v '0.9.10'
+sudo apt-get install -y libxslt-dev libxml2-dev
 
-sudo apt-get install libxslt-dev libxml2-dev
+
+
 
 
 echo "
-############################## Install Repositories ############################
+############################## Install Repositories ###########################
 "
 echo "
 ### Clone, install, and test rag.
@@ -61,13 +106,12 @@ cd ..
 echo "
 ### Clone, install, and test hw.
 "
-git clone https://github.com/saasbook/hw.git
+git clone https://$GH_USER:$GH_PASS@github.com/saasbook/hw.git
 cd hw
 pwd
 git checkout $HW_BRANCH
 git branch -a
 bundle install
-bundle exec rspec
 # TODO update these when develop is merged, functionality moved to hw/features
 #bundle exec cucumber # ./check_all_solutions
 ./check_all_solutions # bundle exec cucumber
@@ -84,17 +128,15 @@ rvm gemset import rag3 # => failed unless export first
 rvm gemset use rag3
 cd ../../../..
 
-
 echo "
 ### Clone, install, saasbook/rottenpotatoes hw3_solution branch.
 "
-git clone https://github.com/saasbook/rottenpotatoes.git
+git clone https://$GH_USER:$GH_PASS@github.com/saasbook/rottenpotatoes.git
+####git clone https://github.com/saasbook/rottenpotatoes.git
 cd rottenpotatoes/
 pwd
-#git checkout -b $ROTTENPOTATOES_BRANCH origin/$ROTTENPOTATOES_BRANCH
-#This should be the same
 git checkout $ROTTENPOTATOES_BRANCH
-# TODO update not needed if Spring2014 branch has updated Gemfile.lock
+# TODO update not needed when Spring2014 branch gets updated Gemfile.lock
 # which will be needed to get rid of ruby-debug and or update beyond current 3.1.0
 #git checkout -tb Spring2014 origin/Spring2014 #bundle update --source therubyracer
 bundle update --source therubyracer
@@ -104,8 +146,39 @@ bundle exec rake db:migrate
 bundle exec rake db:test:prepare
 cd ..
 
+
+
+
+clear
 echo "
-#############################       Run        #################################
+#############################       Edit     ################################
+"
+echo "
+### I will run nano twice to install 2 files in rag/config and edit them.
+### Or you can quit now and edit the files offline because the only other
+### thing left after this is running the screen commands from this file:
+### $0 Line 167 or so.
+
+* Click Enter key to continue, Ctrl+c to exit prematurely.
+"
+read -p "
+* #1 Copy old rag/config/autograders.yml text to clipboard.
+* Click Enter to open nano for the new file, paste in the text to nano. Save and close nano to continue.
+"
+nano rag/config/autograders.yml
+clear
+read -p "
+* #2 Do similar for rag/config/conf.yml. Click Enter to begin.
+"
+nano rag/config/conf.yml
+read -p "** Don't put them in source control. Click Enter key to continue."
+
+
+
+
+
+echo "
+#############################       Run        ################################
 "
 
 echo "
@@ -114,17 +187,8 @@ Resume with: screen -r $SPORK_SCREEN
 "
 cd rottenpotatoes
 pwd
-screen -h 1000 -dmS $SPORK_SCREEN bash -c 'bundle install; bundle exec spork cucumber'
+screen -h $SCROLLBACK -dmS $SPORK_SCREEN bash -c 'bundle install; bundle exec spork cucumber'
 cd ..
-
-
-# TODO Copied manually! ~/hide/rag/config/autograders.yml and conf.yml to rag/config
-echo "
-* Now, copy old rag/config/autograders.yml and conf.yml to rag/config and modify.
-Don't add them to source control.
-"
-read -p "** Click Enter key to continue when done."
-
 
 echo "
 ### Repeat bundle install and launch run_edx_client.rb in rag repo.
@@ -132,18 +196,21 @@ Resume with: screen -r $EDX_CLIENT_SCREEN
 "
 cd rag
 pwd
-screen -h 1000 -dmS $EDX_CLIENT_SCREEN bash -c 'bundle install; while true; do ./run_edx_client.rb live; done'
+screen -h $SCROLLBACK -dmS $EDX_CLIENT_SCREEN bash -c 'bundle install; while true; do ./run_edx_client.rb live; done'
 cd ..
 
+#echo '
+#Attempt to install git-aware bash prompt in ~/.bash_profile
+#'
+#echo '
+#export PS1=$PS1\'\w\[\033[01;34m\]$(__git_ps1 " (%s)")${ret_status}$(~/.rvm/bin/rvm-prompt u) $(~/.rvm/bin/rvm-prompt g)\[\033[00m\]\' ' >> ~/.bash_profile
+#source ~/.bash_profile
 
+screen -ls
 echo "
+Installed with args:$INSTALL_ARGS
 $0 done!
 * Now, check courseware is same queue and assignments as rag/config/autograder.yml
 "
-
-echo '
-export PS1=$PS1\w\[\033[01;34m\]$(__git_ps1 " (%s)")${ret_status}$(~/.rvm/bin/rvm-prompt u) $(~/.rvm/bin/rvm-prompt g)\[\033[00m\] ' >> ~/.bash_profile
-
-source ~/.bash_profile
 
 
