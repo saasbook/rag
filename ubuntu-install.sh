9c43d77ea8ccaad930d2d1dfbbfe5ef1459aac69
Paul McCulloch <hrfeels@gmail.com>
2014-05-21 07:33:34 +0000

    source ~/.bash_profile in screen commands

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Simple autograder setup.sh for configuring Ubuntu 14.04 LTS EC2 instance
+# Simple autograder setup script for configuring Ubuntu 14.04 LTS EC2 instance
 
 
 SPORK_SCREEN=Spork
@@ -9,7 +9,7 @@ RAG_BRANCH=Spring2014
 HW_BRANCH=Spring2014
 ROTTENPOTATOES_BRANCH=hw3_solution
 RUBYGEMS_VERSION=2.2.0
-clear
+#clear
 sleep 1
 
 
@@ -40,11 +40,10 @@ echo "
 read -p "* Enter scrollback for screen. [$SCROLLBACK_DEFAULT]: " SCROLLBACK
 SCROLLBACK="${SCROLLBACK:-$SCROLLBACK_DEFAULT}"
 read -p "* Enter GitHub username for private repos: " GH_USER
-read -s -p "* Enter GitHub password: " GH_PASS
-clear
+read -s -p "* Enter GitHub password (will be hidden): " GH_PASS
+#clear
 
-INSTALL_ARGS="
----------------------------------
+INSTALL_ARGS="---------------------------------
 Install to PWD: $PWD
 RUBYGEMS_VERSION: $RUBYGEMS_VERSION
 GH_USER: $GH_USER
@@ -55,10 +54,12 @@ ROTTENPOTATOES_BRANCH: $ROTTENPOTATOES_BRANCH
 SPORK_SCREEN: $SPORK_SCREEN
 EDX_CLIENT_SCREEN: $EDX_CLIENT_SCREEN
 SCROLLBACK: $SCROLLBACK
----------------------------------
-"
+---------------------------------"
 echo "
-     Confirm install options:$INSTALL_ARGS"
+
+     Confirm install options:
+     $INSTALL_ARGS
+"
 read -p "* If above info is correct, click Enter key to continue, or Ctrl+c to exit and edit $0 "
 
 
@@ -82,7 +83,7 @@ rvm --install use 1.9.3 && rvm rubygems --force $RUBYGEMS_VERSION"
 gem install therubyracer -v '0.9.10'"
 sudo apt-get install -y libxslt-dev libxml2-dev
 
-
+source /home/ubuntu/.bash_profile
 
 
 
@@ -97,7 +98,6 @@ cd rag
 pwd
 git checkout $RAG_BRANCH
 git branch -a
-#bundle update --source ZenTest
 bundle install
 bundle exec rspec
 bundle exec cucumber
@@ -132,7 +132,6 @@ echo "
 ### Clone, install, saasbook/rottenpotatoes hw3_solution branch.
 "
 git clone https://$GH_USER:$GH_PASS@github.com/saasbook/rottenpotatoes.git
-####git clone https://github.com/saasbook/rottenpotatoes.git
 cd rottenpotatoes/
 pwd
 git checkout $ROTTENPOTATOES_BRANCH
@@ -145,10 +144,11 @@ bundle exec rake db:migrate
 bundle exec rake db:test:prepare
 cd ..
 
+source /home/ubuntu/.bash_profile
 
 
 
-clear
+#clear
 echo "
 #############################       Edit     ################################
 "
@@ -166,13 +166,12 @@ read -p "
 * Save and close nano to continue.
 "
 nano rag/config/autograders.yml
-clear
+#clear
 read -p "
 * #2 Do similar for rag/config/conf.yml. Click Enter to begin.
 "
 nano rag/config/conf.yml
 read -p "** Don't put them in source control. Click Enter key to run screens."
-
 
 
 
@@ -187,7 +186,8 @@ Resume with: screen -r $SPORK_SCREEN
 "
 cd rottenpotatoes
 pwd
-screen -h $SCROLLBACK -dmS $SPORK_SCREEN bash -c 'bundle install; bundle exec spork cucumber'
+screen -h $SCROLLBACK -dmS $SPORK_SCREEN bash -c\
+ 'gem -v; bundle install; bundle exec spork cucumber'
 cd ..
 
 echo "
@@ -196,7 +196,8 @@ Resume with: screen -r $EDX_CLIENT_SCREEN
 "
 cd rag
 pwd
-screen -h $SCROLLBACK -dmS $EDX_CLIENT_SCREEN bash -c 'bundle install; while true; do ./run_edx_client.rb live; done'
+screen -h $SCROLLBACK -dmS $EDX_CLIENT_SCREEN bash -c\
+ 'source /home/ubuntu/.bash_profile; gem -v; bundle install; while true; do ./run_edx_client.rb live; done'
 cd ..
 #echo '
 #Attempt to install git-aware bash prompt in ~/.bash_profile
@@ -210,9 +211,12 @@ echo "
 "
 screen -ls
 echo "
-Installed with args:$INSTALL_ARGS
+
+Installed with args:
+$INSTALL_ARGS
+
 $0 done!
-* 'source /home/ubuntu/.rvm/scripts/rvm'
+
 * Now, check courseware is same queue and assignments as
 * in the recently modified ./rag/config/autograders.yml
 "
