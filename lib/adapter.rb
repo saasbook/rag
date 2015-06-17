f663c94e360fd1e6537631bd155450c8d2d09d72
Sean Zhu <sean.zhu@berkeley.edu>
2015-06-16 18:15:44 -0700

    WIP conf loading changes

--- lib/adapter.rb
+++ lib/adapter.rb
@@ -1,13 +1,18 @@
-require_relative 'adapter/xqueue'
+require 'yaml'
 
 module Adapter
-  ALL_BY_NAME = {
-    xqueue: Xqueue
-  }
-
   DEFAULT_NAME = :xqueue
 
-  
+  ADAPTER_NOT_FOUND = "Adapter not found: %s"
+  CONF_FILE_NOT_FOUND = <<-EOS.undent
+    Conf file not found: %s
+    Please copy conf.yml.example into %s and configure the parameters.
+  EOS
+  CONF_KEY_NOT_FOUND = <<-EOS.undent
+    Conf key not found: %s
+    In conf file: %s
+  EOS
+
   # Returns hash of assignment_part_ids to hashes containing uri and grader type
   # i.e. { "assign-1-part-1" => {:uri => 'solutions/part1_spec.rb', :type => 'RspecGrader' } }
   def initialize_autograders(filename)
@@ -19,8 +24,26 @@ module Adapter
     end
   end
 
-  # not too sure if this is valid..... 
+  def new(path, key)
+    conf = load_conf(path, key)
+    get(conf['adapter']).new(conf)
+  end
+
+  private
+
+  def load_conf(path, key)
+    raise CONF_FILE_NOT_FOUND % path, path unless File.file?(path)
+    confs = YAML.load_file(path)
+    confs[key] || (raise CONF_KEY_NOT_FOUND % key, path)
+  end
+
   def get(name = DEFAULT_NAME)
-    ALL[name]
+    case name.lower.to_sym
+    when :xqueue
+      require_relative 'adapter/xqueue'
+      Xqueue
+    else
+      raise ADAPTER_NOT_FOUND % name.inspect
+    end
   end
 end
--- lib/adapter/base.rb
+++ lib/adapter/base.rb
@@ -1,19 +1,8 @@
 require 'yaml'
 
 module Adapter
-
-
   class Base
-    attr_accessor :conf
-    attr_accessor :autograder
-
-    def initialize(path, name)
-      unless File.file?(path)
-        raise "Please copy conf.yml.example into conf.yml and configure the parameters"
-      end
-      confs = YAML.load_file(path)
-      conf = confs[name] || raise "Couldn't load configuration #{conf_name}"
-    end
+    attr_accessor :conf, :autograder
 
     def run
       raise NotImplementedError, "abstract method"
