9fa94c6768512eef4b3fa12300d04a45b4412230
zhangaaron <aaron.zhang@berkeley.edu>
2015-06-30 09:43:26 -0700

    temp commits

--- lib/graders/rspec_grader/rspec_runner.rb
+++ lib/graders/rspec_grader/rspec_runner.rb
@@ -1,66 +1,62 @@
+class RspecRunner               # :nodoc:
+  require 'rubygems'
+  require 'rspec'
+  require 'tempfile'
+  require 'stringio'
 
+  class ExampleTimeoutError < StandardError ; end
 
-module Graders
-  class RspecRunner               # :nodoc:
-    require 'rubygems'
-    require 'rspec'
-    require 'tempfile'
-    require 'stringio'
+  @@preamble = IO.read(File.join(File.expand_path(File.dirname(__FILE__)), 'rspec_sandbox.rb'))
 
-    class ExampleTimeoutError < StandardError ; end
+  attr_reader :total, :passed, :failed, :pending, :output
+  def initialize(code, specfile)
+    @code = code
+    @specs = IO.read(specfile)
+    @total = 0
+    @passed = 0
+    @failed = 0
+    @pending = 0
+    @output = ''
+    @errors = false
+  end
 
-    @@preamble = IO.read(File.join(File.expand_path(File.dirname(__FILE__)), 'rspec_sandbox.rb'))
+  def run
+    @output = run_rspec
+    parse_stats unless @errors
+  end
 
-    attr_reader :total, :passed, :failed, :pending, :output
-    def initialize(code, specfile)
-      @code = code
-      @specs = IO.read(specfile)
-      @total = 0
-      @passed = 0
-      @failed = 0
-      @pending = 0
-      @output = ''
-      @errors = false
-    end
+  private
 
-    def run
-      @output = run_rspec
-      parse_stats unless @errors
-    end
-
-    private
-
-    def run_rspec
-      errs = StringIO.new('', 'w')
-      output = StringIO.new('', 'w')
-      Tempfile.open(['rspec', '.rb']) do |file|
-        begin
-          # don't put anything before student code, so line numbers are preserved
-          file.write(@code)
-          # sandbox the code with timeouts
-          file.write(@@preamble)
-          # the specs that go with this code
-          file.write(@specs)
-          file.flush
-          RSpec::Core::Runner::run([file.path], errs, output)
-        rescue Exception => e
-          # if tmpfile name appears in err msg, replace with 'your_code.rb' to be friendly
-          output.string << e.message.gsub(file.path, 'your_code.rb')
-          @errors = true
-        end
+  def run_rspec
+    ## TODO: USE THE JSON FORMATTER TO COMPUTE SECORES AND STUFF; LESS FRAGILE
+    errs = StringIO.new('', 'w')
+    output = StringIO.new('', 'w')
+    Tempfile.open(['rspec', '.rb']) do |file|
+      begin
+        # don't put anything before student code, so line numbers are preserved
+        file.write(@code)
+        # sandbox the code with timeouts
+        file.write(@@preamble)
+        # the specs that go with this code
+        file.write(@specs)
+        file.flush
+        RSpec::Core::Runner::run([file.path], errs, output)
+      rescue Exception => e
+        # if tmpfile name appears in err msg, replace with 'your_code.rb' to be friendly
+        output.string << e.message.gsub(file.path, 'your_code.rb')
+        @errors = true
       end
-      return [output.string, errs.string].join("\n")
     end
+    return [output.string, errs.string].join("\n")
+  end
 
-    def parse_stats
-      regex = /(\d+)\s+examples?,\s+(\d+)\s+failures?(,\s+(\d+)\s+pending)?$/
-      if @output.force_encoding('us-ascii').encode('utf-8', :invalid => :replace, :undef => :replace, :replace => '?') =~ regex
-        @total, @failed, @pending = $1.to_i, $2.to_i, $4.to_i
-        @passed = @total - @failed - @pending
-
-      else
-        @output << "\nCan't parse output: #{@output_stream}"
-      end
+  def parse_stats
+    regex = /(\d+)\s+examples?,\s+(\d+)\s+failures?(,\s+(\d+)\s+pending)?$/
+    if @output.force_encoding('us-ascii').encode('utf-8', :invalid => :replace, :undef => :replace, :replace => '?') =~ regex
+      @total, @failed, @pending = $1.to_i, $2.to_i, $4.to_i
+      @passed = @total - @failed - @pending
+    else
+      @output << "\nCan't parse output: #{@output_stream}"
     end
   end
 end
