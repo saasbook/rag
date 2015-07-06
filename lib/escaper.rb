fac0afb106cdff67057f78ac8fe62342b368f613
zhangaaron <aaron.zhang@berkeley.edu>
2015-07-06 16:00:43 -0700

    update to new xqueue-ruby version

--- lib/escaper.rb
+++ lib/escaper.rb
@@ -2,5 +2,5 @@ def escape_all_fields(str)
   chomped_str = str.chomp
   split_str = chomped_str.split
   split_str.map! {|x| '"' + x + '"'}
-  return_str = split_str.join(" ")
+  split_str.join(" ")
 end
