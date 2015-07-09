3610355d71285d587c4b43f0321741a38a59b29a
zhangaaron <aaron.zhang@berkeley.edu>
2015-07-09 15:33:24 -0700

    delete weighted rspec grader, change references of github and heroku grader to now point to base rspec_grader

--- lib/graders/rspec_grader/weighted_rspec_grader.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'rspec_grader'
-module Graders
-  class WeightedRspecGrader < RspecGrader
-    def grade
-      ## TEMP REFACTOR, EVENTUALLY DEPRECATE
-      super(true)
-    end
-  end
-end
