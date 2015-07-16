4848ca587de791f04e0ba623c28302e3c258ef43
JUCHAN KIM <yj1877@berkeley.edu>
2015-07-16 12:57:24 -0700

    added hw1.rb again

---
+++ features/support/hw1.rb
@@ -0,0 +1,17 @@
+# Ruby intro part 1 solutions
+# Returns the sum of all the numbers in `collection`, which must be
+# enumerable
+def sum(collection)
+  collection.inject(0) do |total, n|
+    total + n
+  end
+end
+
+# Return the sum of the 2 largest elements in a collection
+def max_2_sum(collection)
+end
+
+# Return true iff exactly 2 elements of collection sum to the given number
+# Uses the handy `permutation` instance method of `Array`.
+def sum_to_n?(collection, total)
+end
