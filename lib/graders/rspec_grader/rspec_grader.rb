e49728947b7711f4b0a8dd584bce15ded9a1f966
JUCHAN KIM <yj1877@berkeley.edu>
2015-07-16 12:54:58 -0700

    fixed the IOerror by adding tty

--- lib/graders/rspec_grader/rspec_grader.rb
+++ lib/graders/rspec_grader/rspec_grader.rb
@@ -24,6 +24,7 @@ module Graders
     def grade(weighted=false)
       run_in_thread(runner_block)
       # run_in_subprocess(runner_block)
+      {raw_score: @raw_score, raw_max: @raw_max, comments: @comments}
     end
     
     def compute_points (file_path)
@@ -32,14 +33,20 @@ module Graders
       points_max = 0
       points = 0
       RSpec.configure do |config|
+        config.color = true
+        config.tty = true
+
         config.formatter = 'documentation'
         config.formatter = 'RSpec::Core::Formatters::JsonPointsFormatter'
-        config.color = true
+
         config.output_stream = File.open('rspec_output.txt', 'wb')
         # getting rid of deprecation warnings
         config.expect_with(:rspec) { |cc| cc.syntax = [:should, :expect] }
+        config.deprecation_stream = File.open('deprecations.txt', 'w')
       end
+
       RSpec::Core::Runner.run([file_path], errs, output)
+      #raise "ho------------------------"
       formatter = RSpec.configuration.formatters.select {|formatter| formatter.is_a? RSpec::Core::Formatters::JsonPointsFormatter}.first
       output_hash = formatter.output_hash
       output_hash[:examples].each do |example|
@@ -54,12 +61,11 @@ module Graders
         # raise "#{@submission_path}"
         Graders.load_student_files(@submission_path)
         RSpec.reset
-        raw_score, raw_max, comments = compute_points(@spec_file_path)
+        @raw_score, @raw_max, @comments = compute_points(@spec_file_path)
       rescue Exception => e
         puts 'When does this happen?'
         raise e
       end
-      {raw_score: raw_score, raw_max: raw_max, comments: comments}
     end
   end
 end
