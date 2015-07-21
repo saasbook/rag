8b420c27fa302b3069e1aa31277ceaa99b86e2e9
zhangaaron <aaron.zhang@berkeley.edu>
2015-07-20 18:10:43 -0700

    fixed issue with heroku grader

--- lib/graders/rspec_grader/heroku_rspec_grader.rb
+++ lib/graders/rspec_grader/heroku_rspec_grader.rb
@@ -7,20 +7,12 @@ module Graders
       @timeout = 60
       heroku_file = Dir[File.join(@submission_path, '*.rb')].first  # there should only be on file submitted if its a heroku submission.
       @heroku_uri = IO.read(heroku_file).strip
+      @load_student_files = false
     end
 
     def grade
       ENV['HEROKU_URI'] = @heroku_uri
       super
-    end
-
-    def runner_block
-      begin
-        raw_score, raw_max, comments = compute_points(@spec_file_path)
-      rescue Exception => e
-        raise e
-      end
-      @output_hash = {raw_score: raw_score, raw_max: raw_max, comments: comments}
     end
   end
 end
