e7248dd503aae392ef3d16f2283e822a9e2e9347
JUCHAN KIM <yj1877@berkeley.edu>
2015-08-23 18:28:05 -0700

    travis check

--- lib/rag_logger.rb
+++ lib/rag_logger.rb
@@ -3,12 +3,9 @@ require 'logger'
 module RagLogger
 
   #Create a singleton logger used across all rag classes. Write to a specified log file, if already exists, create a new file w/ timestamp in file name
-  def self.configure_logger(log_to_file, level)
-    if log_to_file
-      @@logger = Logger.new(File.open("logs/log-#{Time.now.to_s}", 'w'))
-    else
-      @@logger = Logger.new(STDOUT)
-    end
+  def self.configure_logger(output_file, level)
+    log_file = File.exist?(output_file) ? File.open(output_file + Time.now.to_s, 'w') : File.open(output_file, 'w')
+    @@logger = Logger.new(log_file)
     @@logger.level = level
   end
 
