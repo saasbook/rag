b6e81bac8784524b2cc0797d6a3321b1e1a2b3b1
Sam Joseph <tansaku@gmail.com>
2018-07-09 10:44:47 +0100

    switch to matching heroku style

--- lib/graders/rspec_grader/github_rspec_grader.rb
+++ lib/graders/rspec_grader/github_rspec_grader.rb
@@ -4,11 +4,11 @@ require_relative 'rspec_grader'
 module Graders
   class GithubRspecGrader < RspecGrader
     def initialize(submission_path, assignment)
-      super('', assignment)
+      super(submission_path, assignment)
       @timeout = 180
       @load_student_files = false
-      username = File.open(submission_path, &:readline)
-      ENV['GITHUB_USERNAME'] = username.strip.delete("\n")
+      github_file = Dir[File.join(@submission_path, '*')].first  # there should only be one file submitted if its a github submission.
+      ENV['GITHUB_USERNAME'] = IO.read(github_file).strip.delete("\n")
       # http://github.com/username/assignment_name is the uri?
     end
   end
