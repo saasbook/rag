c46df186be31271c0d20b2ba10a59d7036dc114c
Sam Joseph <tansaku@gmail.com>
2018-07-09 10:40:01 +0100

    update github rspec grader to work with submission path

--- lib/graders/rspec_grader/github_rspec_grader.rb
+++ lib/graders/rspec_grader/github_rspec_grader.rb
@@ -3,10 +3,11 @@ require_relative 'rspec_grader'
 
 module Graders
   class GithubRspecGrader < RspecGrader
-    def initialize(username, assignment)
+    def initialize(submission_path, assignment)
       super('', assignment)
       @timeout = 180
       @load_student_files = false
+      username = File.open(submission_path, &:readline)
       ENV['GITHUB_USERNAME'] = username.strip.delete("\n")
       # http://github.com/username/assignment_name is the uri?
     end
